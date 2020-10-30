6a9eeb84d06e3ff8b38ae1c287e4b1b4d3b86fcf
Benjamin Dehli <superelg@gmail.com>
2020-10-30 13:46:45 +0100

    Remove console logging

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = function override(config, env) {
-    console.log(config);
     const configPath = process.env.NODE_ENV === 'development' ? 'src/config/config.local.json' : 'src/config/config.json';
     config.plugins.push(new CopyWebpackPlugin(
         {
