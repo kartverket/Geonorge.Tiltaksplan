cb95f9a818da0929d031f40ec8b25aab564b1fa7
Benjamin Dehli <superelg@gmail.com>
2020-10-30 13:48:51 +0100

    Use environment variable in navigation bar

--- src/components/partials/NavigationBar.js
+++ src/components/partials/NavigationBar.js
@@ -3,11 +3,14 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import '@kartverket/geonorge-web-components/MainNavigation';
 
+// Helpers
+import {getEnvironmentVariable} from 'helpers/environmentVariableHelpers.js';
 
 class NavigationBar extends Component {
 
   render() {
-    return (<main-navigation environment="dev"></main-navigation>)
+    const environment = getEnvironmentVariable('environment');
+    return (<main-navigation environment={environment}></main-navigation>)
   }
 }
 
