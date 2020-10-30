e43014ec4c194dfc54003e62955f7539988a56bd
Ole <ole@arkitektum.no>
2020-10-30 08:23:22 +0100

    Listview for tiltaksplaner

--- src/components/partials/MeasuresList/MeasuresListElement.js
+++ src/components/partials/MeasuresList/MeasuresListElement.js
@@ -4,24 +4,41 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 // Stylesheets
+import style from 'components/partials/MeasuresList/MeasuresListElement.module.scss';
 
 
 class MeasuresListElement extends Component {
 
+  renderTrafficClassName(measure) {
+    switch(measure) {
+      case 1:
+      return 'greencolor';
+      case 2:
+      return 'yellowcolor';
+      case 3:
+        return 'redcolor';
+      default:
+      return '';
+    }
+  }
+
   render() {
+    
     const measure = this.props.measure;
-    console.log('fisk' + measure)
+    console.log(this.renderTrafficClassName(measure.trafficLight))
     return this.props.measure
-      ? (<div>
-        {measure.name}
-        {measure.progress}
-        {measure.volume}
-        {measure.status}
-        {measure.trafficLight}
-        {measure.results}
-        {measure.comment}
+    
+      ? (<div className={style.row}>
+        <div>{measure.name}</div>
+        <div>{measure.progress}</div>
+        <div>{measure.volume}</div>
+        <div>{measure.status}</div>
+        <div className={`${style.trafficlight} ${style[this.renderTrafficClassName(measure.trafficLight)]}`}> {measure.trafficLight}</div>
+        <div>{measure.results}</div>
+        <div>{measure.comment}</div>
       </div>)
       : '';
+      
   }
 }
 
