46e5df23d0eae86b7a2770583039e7ef3ba8a0b6
Ole <ole@arkitektum.no>
2020-11-10 10:43:08 +0100

    refactor for table of activites

---
+++ src/components/partials/ActivityTable.js
@@ -0,0 +1,43 @@
+// Dependencies
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ActivityTableRow from 'components/partials/ActivityTable/ActivityTableRow';
+import style from 'components/partials/ActivityTable.module.scss'
+
+class ActivityTable extends Component {
+      
+   renderActivityTableRows(activities) {
+      if (!activities || !activities.length) {
+         return '';
+      }
+
+      return (
+         <table className={style.activitiesTable}>
+         <thead>
+             <tr>
+                 <th>Navn</th>
+                 <th>Beskrivelse</th>
+                 <th>Deltakere</th>
+                 <th>Status</th>
+                 <th>Start</th>
+                 <th>Slutt</th>
+            </tr>
+         </thead>
+         <tbody>
+         {activities.map(activity => <ActivityTableRow key={activity.id} activity={activity} />)}
+         </tbody>
+         </table>
+      );
+   }
+
+   render() {
+      const activities = this.props.activities;
+        
+      return this.renderActivityTableRows(activities)
+   }
+}
+
+const mapStateToProps = state => ({ activities: state.activities });
+
+
+export default connect(null, null)(ActivityTable);
--- src/components/routes/Measure.js
+++ src/components/routes/Measure.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Container from 'components/template/Container';
 import { fetchSelectedMeasure } from 'actions/MeasureActions';
-import DayJS from 'react-dayjs';
 import style from 'components/routes/Measure.module.scss';
 import Activity from 'components/partials/ActivityTable/ActivityTableRow';
+import ActivityTable from 'components/partials/ActivityTable';
 
 
 class Measure extends Component {
@@ -17,31 +17,15 @@ class Measure extends Component {
         return this.props.match && this.props.match.params && this.props.match.params.measureId
         ? this.props.match.params.measureId
         : null;
-    }
-
-    renderActivities(activities){
-
-        return activities && activities.length ?activities.map(activity => {
-            return ( 
-                <Activity key={activity.id} activity={activity} />
-                 )
-        }) : null;
-    }
+    }   
 
     render() {
         const selectedMeasure = this.props.selectedMeasure;
-        return selectedMeasure ? (<Container>
+        return selectedMeasure ? (<Container>            
             <h1>{selectedMeasure.name}</h1>
         <div>{selectedMeasure.progress}</div>
         <div>{selectedMeasure.results}</div>
-            <table className={style.activitiesTable}>
-                <thead>
-                    <tr><th>Navn</th><th>Beskrivelse</th><th>Deltakere</th><th>Status</th><th>Start</th><th>Slutt</th></tr>
-                </thead>
-                <tbody>
-                {this.renderActivities(selectedMeasure.activities)}
-                </tbody>
-                </table>
+        <ActivityTable activities={selectedMeasure.activities} />            
         </Container>) : ''
     }
 }
