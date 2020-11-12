7e0fc5ed6198d550efbdd4fc197f4e7cd4d9543b
Benjamin Dehli <superelg@gmail.com>
2020-11-12 09:39:56 +0100

    Add save button handling for activity modal

--- src/components/routes/Measure.js
+++ src/components/routes/Measure.js
@@ -2,76 +2,92 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Container from 'components/template/Container';
-import { fetchSelectedMeasure } from 'actions/MeasureActions';
-import style from 'components/routes/Measure.module.scss';
-import ActivityTable from 'components/partials/ActivityTable';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
+
+
+// Components
+import ActivityTable from 'components/partials/ActivityTable';
+
+// Actions
+import { fetchSelectedMeasure } from 'actions/MeasureActions';
+import { createActivity } from 'actions/ActivityActions';
+
+// Stylesheets
+import style from 'components/routes/Measure.module.scss';
+
+
 import { Activity } from 'models/activity';
 
 
 class Measure extends Component {
     constructor(props) {
         super(props);
-  
+
         this.state = {
-           modalOpen: false,         
+            modalOpen: false,
         };
-  
+
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
+        this.saveModal = this.saveModal.bind(this);
         this.handleChange = this.handleChange.bind(this);
-  
-     }
 
-     openModal() {
+    }
+
+    openModal() {
         this.setState({
-           modalOpen: true,
-           newActivity: new Activity()
+            modalOpen: true,
+            newActivity: new Activity()
         });
-     }
-  
-     handleChange(event) {
+    }
+
+    handleChange(event) {
         const newActivity = this.state.newActivity;
         newActivity[event.target.name] = event.target.value;
-  
+
         this.setState({ newActivity }, () => console.log(this.state.newActivity));
-     }
-  
-     closeModal() {
+    }
+
+    closeModal() {
         this.setState({ modalOpen: false });
-     }
-  
-     /*const */
+    }
+
+    saveModal() {
+        this.props.createActivity(this.state.newActivity).then(() => {
+            this.closeModal();
+        });
+    }
+
     componentDidMount() {
         this.props.fetchSelectedMeasure(this.getSelectedMeasureId())
     }
 
-    getSelectedMeasureId(){
+    getSelectedMeasureId() {
         return this.props.match && this.props.match.params && this.props.match.params.measureId
-        ? this.props.match.params.measureId
-        : null;
-    }   
+            ? this.props.match.params.measureId
+            : null;
+    }
 
     render() {
         const selectedMeasure = this.props.selectedMeasure;
-        return selectedMeasure ? (<Container>            
+        return selectedMeasure ? (<Container>
             <h1>{selectedMeasure.name}</h1>
-        <div className={style.block}>{selectedMeasure.progress}</div>
-        <div className={style.block}>{selectedMeasure.results}</div>
+            <div className={style.block}>{selectedMeasure.progress}</div>
+            <div className={style.block}>{selectedMeasure.results}</div>
 
-        <div>
-            <button className="btn" onClick={this.openModal}>Legg til aktivitet</button>
-        </div>
+            <div>
+                <button className="btn" onClick={this.openModal}>Legg til aktivitet</button>
+            </div>
 
-        <ActivityTable activities={selectedMeasure.activities} />   
-        {this.state.newActivity ? (
-            <Dialog open={this.state.modalOpen} onClose={this.closeModal} aria-labelledby="form-dialog-title">
-            <DialogTitle id="form-dialog-title">Legg til aktivitet</DialogTitle>
-            <DialogContent>
+            <ActivityTable activities={selectedMeasure.activities} />
+            {this.state.newActivity ? (
+                <Dialog open={this.state.modalOpen} onClose={this.closeModal} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Legg til aktivitet</DialogTitle>
+                    <DialogContent>
 
                <div className={style.block}><TextField label="Navn" variant="outlined" name="name" value={this.state.newActivity.name} onChange={this.handleChange} /></div>
                <div className={style.block}><TextField label="Tittel" variant="outlined" name="title" value={this.state.newActivity.title} onChange={this.handleChange} /></div>
@@ -98,7 +114,7 @@ class Measure extends Component {
             </DialogContent>
             <DialogActions>
                <button className="btn" onClick={this.closeModal}>Lukk</button>
-               <button className="btn primary" onClick={this.closeModal}>Lagre</button>
+               <button className="btn primary" onClick={this.saveModal}>Lagre</button>
             </DialogActions>
          </Dialog>  
         ) : ''}
@@ -112,7 +128,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-    fetchSelectedMeasure
+    fetchSelectedMeasure,
+    createActivity
 };
 
 
