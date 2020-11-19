4bad7f222fb8afb08c6f03629c6de5eed7fc4bb0
Benjamin Dehli <superelg@gmail.com>
2020-11-19 11:28:07 +0100

    Add owner property to measure model

--- src/models/measure.js
+++ src/models/measure.js
@@ -1,6 +1,7 @@
 export class Measure {
-   constructor({ id, name, progress, volume, status, trafficLight, comment, results } = {}) {
+   constructor({ id, owner, name, progress, volume, status, trafficLight, comment, results } = {}) {
       this.id = id || 0;
+      this.owner = owner || {};
       this.name = name || '';
       this.progress = progress || '';
       this.volume = volume || 0;
