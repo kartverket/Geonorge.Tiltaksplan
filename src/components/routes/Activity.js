4c1a2a2597742ece50bb186eb2d789d8e6af7251
Benjamin Dehli <superelg@gmail.com>
2020-11-25 17:12:52 +0100

    Add route component for activity

---
+++ src/components/routes/Activity.js
@@ -0,0 +1,56 @@
+// Dependencies
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Components
+import Container from 'components/template/Container';
+import EditActivity from 'components/partials/EditActivity';
+
+// Actions
+import { fetchActivity } from 'actions/ActivityActions';
+
+class Activity extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataFetched: false
+        };
+    }
+
+    componentDidMount() {
+        this.props.fetchActivity(this.getActivityId())
+            .then(() => {
+                this.setState({ dataFetched: true });
+            });
+    }
+
+    getActivityId() {
+        return this.props.match && this.props.match.params && this.props.match.params.activityId
+            ? this.props.match.params.activityId
+            : null;
+    }
+
+    render() {
+        if (!this.state.dataFetched) {
+            return '';
+        }
+        return (
+            <Container>
+                <h1>{this.props.activity.name}</h1>
+                <EditActivity />
+            </Container>
+        );
+    }
+}
+
+
+const mapStateToProps = (state) => ({
+    activity: state.selectedActivity
+ });
+ 
+ const mapDispatchToProps = {
+    fetchActivity
+ };
+ 
+
+export default connect(mapStateToProps, mapDispatchToProps)(Activity);
