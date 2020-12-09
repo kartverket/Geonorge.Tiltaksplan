fa56d85a0431ed58b3232d83867cac0e38a15e26
Benjamin Dehli <superelg@gmail.com>
2020-12-09 13:30:08 +0100

    Add authentication functions to main navigation

--- src/components/partials/NavigationBar.js
+++ src/components/partials/NavigationBar.js
@@ -1,12 +1,33 @@
 // Dependencies
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import '@kartverket/geonorge-web-components/MainNavigation';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { MainNavigation } from '@kartverket/geonorge-web-components/MainNavigation';
+
+
+// Utils
+import userManager from 'utils/userManager';
 
 // Helpers
-import {getEnvironmentVariable} from 'helpers/environmentVariableHelpers.js';
+import { getEnvironmentVariable } from 'helpers/environmentVariableHelpers.js';
 
 class NavigationBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    MainNavigation.setup('main-navigation', {
+      onSignInClick: () => {
+        userManager.signinRedirect();
+      },
+      onSignOutClick: () => {
+        userManager.signoutRedirect({ 'id_token_hint': this.props.user.id_token });
+        userManager.removeUser();
+      }
+    })
+  }
 
   render() {
     const environment = getEnvironmentVariable('environment');
