2877a1fe9a087485d48815346502a31ff5783afb
Benjamin Dehli <superelg@gmail.com>
2020-12-21 12:37:59 +0100

    Add helpers for authorizaton check

--- src/helpers/authorizationHelpers.js
+++ src/helpers/authorizationHelpers.js
@@ -1,11 +1,32 @@
-export const canAddMeasure = () => {
+export const hasAdminRole = authInfo => {
+  return authInfo && authInfo.roles && authInfo.roles.includes('nd.metadata_admin');
+}
+
+export const hasEditorRole = authInfo => {
+  return authInfo && authInfo.roles && authInfo.roles.includes('nd.metadata_editor');
+}
+
+// Measure
+export const canAddMeasure = authInfo => {
+  return hasAdminRole(authInfo);
+}
+
+export const canDeleteMeasure = authInfo => {
+  return hasAdminRole(authInfo);
+}
+
+export const canEditMeasure = authInfo => {
+  return hasAdminRole(authInfo);
+}
+
+
+// Measure Report
+export const canEditReport = () => {
   return true;
 }
 
-export const canEditMeasure = () => {
-  return true;
-}
 
+// Measure Activity
 export const canAddActivity = () => {
   return true;
 }
