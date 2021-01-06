43465e34da7a75a5b302313b31062e8a1cce7533
Benjamin Dehli <superelg@gmail.com>
2021-01-06 10:40:26 +0100

    Add get-action for translations

--- src/actions/ConfigActions.js
+++ src/actions/ConfigActions.js
@@ -3,3 +3,17 @@ import { UPDATE_CONFIG } from 'constants/types';
 export const updateConfig = (config) => dispatch => {
   dispatch({ type: UPDATE_CONFIG, payload: config });
 }
+
+export const translate = (resourceKey, language) => (dispatch, getState) => {
+  const store = getState();
+  const translations = store.config.translations;
+  language = language ? language : store.selectedLanguage;
+
+  const translationsForLanguage = translations.find(translation => {
+    return translation.culture === language;
+  });
+
+  return translationsForLanguage && translationsForLanguage.texts && translationsForLanguage.texts[resourceKey]
+    ? translationsForLanguage.texts[resourceKey]
+    : null
+}
