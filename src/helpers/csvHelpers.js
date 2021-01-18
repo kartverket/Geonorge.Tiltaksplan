060e312a5bb7fa45c8e10fe3d950f057bf7cdfe3
Benjamin Dehli <superelg@gmail.com>
2021-01-18 09:29:57 +0100

    Add helpers for CSV conversion

---
+++ src/helpers/csvHelpers.js
@@ -0,0 +1,23 @@
+
+const convertSelectedPropertiesToCSV = (properties, dataObject) => {
+    return properties.map(property => {
+        const propertyValue = dataObject[property];
+        return typeof propertyValue === 'string' ? `\"${propertyValue}\"` : propertyValue;
+    }).join(',');
+}
+
+const convertObjectArrayToCSV = (properties, objectArray) => {
+    const headers = properties.join(',');
+    const values = objectArray.map(dataObject => {
+        const csvLine = convertSelectedPropertiesToCSV(properties, dataObject);
+        return csvLine;
+    }).join('\r\n');
+    return `${headers}\r\n${values}`;
+}
+
+export const convertMeasureReportsToCSV = (objArray = []) => {
+    const properties = [
+        'id', 'no', 'progress', 'volume', 'status', 'trafficLights', 'results', 'comment'
+    ];
+    return convertObjectArrayToCSV(properties, objArray);
+}
