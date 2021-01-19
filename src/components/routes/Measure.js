ae912e2f1385c858544d134bed7f3734b9bed006
Benjamin Dehli <superelg@gmail.com>
2021-01-19 14:11:02 +0100

    Fix logic for delete measure

--- src/components/routes/Measure.js
+++ src/components/routes/Measure.js
@@ -53,7 +53,7 @@ class Measure extends Component {
    }
 
    handleDeleteMeasure() {
-      this.props.deleteMeasure(this.state.measure)
+      this.props.deleteMeasure(this.props.measure, this.props.user)
          .then(() => {
             this.props.history.push(`/`);
          });
@@ -66,82 +66,78 @@ class Measure extends Component {
    }
 
    render() {
-      if (!this.state.dataFetched) {
-         return '';
-      }
-
-      return (
-         <Container>
-            <h1>{this.props.measure.no} - {this.props.measure.name}</h1>
-            {this.props.measure.infoUrl ?
-               <a href={`${this.props.measure.infoUrl}`}>{this.props.translate('infoLinkMeasure')}</a> : ''
-            }
-            <h5>{this.props.translate('OwnsBy')} {this.props.measure.owner.name}</h5>
-            {
-               canDeleteMeasure(this.props.authInfo)
-                  ? <Button className="mr-2" variant="secondary" onClick={this.openDeleteMeasureModal}>Slett tiltaket</Button>
-                  : ''
-            }
-            <MeasureDetails selectedMeasure={this.props.measure} />
-            <div className={style.btn} onClick={() => { this.setState({ open: !this.state.open }) }}>
-               {this.state.open ? `${this.props.translate('ReportLinkClose')}` : `${this.props.translate('ReportLink')}`} {<FontAwesomeIcon icon={this.state.open ? 'minus-circle' : 'plus-circle'} />}
-            </div>
+      const hasMeasure = this.props.measure && Object.keys(this.props.measure).length;
+      return this.state.dataFetched && hasMeasure
+         ? (
+            <Container>
+               <h1>{this.props.measure.no} - {this.props.measure.name}</h1>
+               {this.props.measure.infoUrl ?
+                  <a href={`${this.props.measure.infoUrl}`}>{this.props.translate('infoLinkMeasure')}</a> : ''
+               }
+               <h5>{this.props.translate('OwnsBy')} {this.props.measure.owner.name}</h5>
+               {
+                  canDeleteMeasure(this.props.authInfo)
+                     ? <Button className="mr-2" variant="secondary" onClick={this.openDeleteMeasureModal}>Slett tiltaket</Button>
+                     : ''
+               }
+               <MeasureDetails selectedMeasure={this.props.measure} />
+               <div className={style.btn} onClick={() => { this.setState({ open: !this.state.open }) }}>
+                  {this.state.open ? `${this.props.translate('ReportLinkClose')}` : `${this.props.translate('ReportLink')}`} {<FontAwesomeIcon icon={this.state.open ? 'minus-circle' : 'plus-circle'} />}
+               </div>
 
 
-            <div className={`${style.reporting} ` + `${this.state.open ? `${style.reportOpen}` : `${style.reportClose}`}`}>
-               <h2>{this.props.translate('progressReportTitle')}</h2>
-               <p>{this.props.translate('lastUpdate')} <DayJS format="DD.MM YYYY" locale="nb">{this.props.measure.lastUpdated}</DayJS></p>
+               <div className={`${style.reporting} ` + `${this.state.open ? `${style.reportOpen}` : `${style.reportClose}`}`}>
+                  <h2>{this.props.translate('progressReportTitle')}</h2>
+                  <p>{this.props.translate('lastUpdate')} <DayJS format="DD.MM YYYY" locale="nb">{this.props.measure.lastUpdated}</DayJS></p>
 
-               <ReportDetails />
-            </div>
+                  <ReportDetails />
+               </div>
 
-            <h2>{this.props.translate('MeasureActivitiesTitle')}</h2>
-
-
-            <ActivityTable activities={this.props.measure.activities} />
-            {
-               canAddActivity(this.props.authInfo)
-                  ? (<div className={style.block}>
-                     <Link to={`${this.getMeasureId()}/ny-aktivitet`}>
-                        <button className="btn btn-primary">{this.props.translate('btnCreate')}</button>
-                     </Link>
-                  </div>)
-                  : ''
-            }
+               <h2>{this.props.translate('MeasureActivitiesTitle')}</h2>
 
 
+               <ActivityTable activities={this.props.measure.activities} />
+               {
+                  canAddActivity(this.props.authInfo)
+                     ? (<div className={style.block}>
+                        <Link to={`${this.getMeasureId()}/ny-aktivitet`}>
+                           <button className="btn btn-primary">{this.props.translate('btnCreate')}</button>
+                        </Link>
+                     </div>)
+                     : ''
+               }
 
+               <Modal
+                  show={this.state.deleteMeasureModalOpen}
+                  onHide={this.closeDeleteMeasureModal}
+                  keyboard={false}
+                  animation={false}
+                  centered
+                  backdrop="static"
+                  aria-labelledby="form-dialog-title">
+                  <Modal.Header closeButton>
+                     <Modal.Title>Slett tiltak</Modal.Title>
+                  </Modal.Header>
 
+                  <Modal.Body>
+                     <p>Er du sikker på at du vil slette {this.props.measure.name}?</p>
+                     {this.props.measure.activities.length > 0 ? 'Du kan ikke slette da det er aktiviteter knyttet til tiltaket' + this.props.measure.name : ''}
+                  </Modal.Body>
 
-            <Modal
-               show={this.state.deleteMeasureModalOpen}
-               onHide={this.closeDeleteMeasureModal}
-               keyboard={false}
-               animation={false}
-               centered
-               backdrop="static"
-               aria-labelledby="form-dialog-title">
-               <Modal.Header closeButton>
-                  <Modal.Title>Slett tiltak</Modal.Title>
-               </Modal.Header>
-
-               <Modal.Body>
-                  <p>Er du sikker på at du vil slette {this.props.measure.name}?</p>
-                  {this.props.measure.activities.length > 0 ? 'Du kan ikke slette da det er aktiviteter knyttet til tiltaket' + this.props.measure.name : ''}
-               </Modal.Body>
-
-               <Modal.Footer>
-                  <Button variant="secondary" onClick={this.closeDeleteMeasureModal}>Avbryt</Button>
-                  <Button disabled={this.props.measure.activities.length > 0} variant="danger" onClick={this.handleDelete}>Slett</Button>
-               </Modal.Footer>
-            </Modal>
-         </Container>
-      );
+                  <Modal.Footer>
+                     <Button variant="secondary" onClick={this.closeDeleteMeasureModal}>Avbryt</Button>
+                     <Button disabled={this.props.measure.activities.length > 0} variant="danger" onClick={this.handleDeleteMeasure}>Slett</Button>
+                  </Modal.Footer>
+               </Modal>
+            </Container>
+         )
+         : '';
    }
 }
 
 const mapStateToProps = (state) => ({
    measure: state.measures.selectedMeasure,
+   user: state.oidc.user,
    authInfo: state.authInfo,
    selectedLanguage: state.selectedLanguage
 });
