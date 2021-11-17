04b354c23794b7628e817a836c2d5fdc103072ef
Dag Olav Dahle <dagolav@arkitektum.no>
2021-11-17 15:49:13 +0100

    Add lastUpdated to csv

--- src/helpers/csvHelpers.js
+++ src/helpers/csvHelpers.js
@@ -38,10 +38,21 @@ export const convertMeasureReportsToCSV = (measureReports = [], options) => {
             "status": getOptionLabelFromValue(options, 'planStatuses', measureReport.status),
             "trafikklys": getOptionLabelFromValue(options, 'trafficLights', measureReport.trafficLight),
             "konkrete resultater": getOptionLabelFromValue(options, 'measureResults', measureReport.results),
-            "kommentar": FixSpecialCharacters(measureReport.comment)
+            "kommentar": FixSpecialCharacters(measureReport.comment),
+            "Oppdatert dato rapport": FormatDate(measureReport.lastUpdated),
+            "Oppdatert dato aktivitet": FormatDate(measureReport.lastUpdatedActivity)
         }
     });
     return convertObjectArrayToCSV(objectArray);
+}
+
+
+function FormatDate (date) {
+    if (date !== null) {
+    const dateArray = date.split("-");
+    const day = dateArray[2].substring(0, 4).split("T");
+        return day[0] + "." + dateArray[1] + "." + dateArray[0];
+    }
 }
 
 function FixSpecialCharacters(text) {
