011603edeb8f581afcb585b82629d25226baf70e
Dag Olav Dahle <dagolav@arkitektum.no>
2022-01-28 14:35:20 +0100

    Display measure.lastUpdated if newer then activity

--- src/components/partials/MeasuresTable/MeasuresTableRow.js
+++ src/components/partials/MeasuresTable/MeasuresTableRow.js
@@ -53,7 +53,7 @@ class MeasuresTableRow extends Component {
             <td data-label={this.props.translate('Measure')} data-tip="Overordnet beskrivelse av tiltaket"><a href={measure.infoUrl} target="_blank">{measure.name}</a></td>
             <td data-label="Status" data-tip="Viser fremdrift på tiltaket">{this.getMeasureStatusLabel(this.props.planStatuses, measure)}</td>
             <td data-label={this.props.translate('Owner')} data-tip="Hovedansvarlig for gjennomføring av tiltaket">{measure.owner.name}</td>
-            <td data-label="Sist oppdatert aktivitet" data-tip="Sist oppdatert aktivitet/rapport">{this.formatDate(measure.lastUpdatedActivity)}</td>
+            <td data-label="Sist oppdatert aktivitet" data-tip="Sist oppdatert aktivitet/rapport">{this.formatDate(measure.lastUpdatedActivity > measure.lastUpdated ? measure.lastUpdatedActivity : measure.lastUpdated)}</td>
             <td data-label="Link til aktivitet" style={{textAlign: 'center', cursor: 'pointer'}}><FontAwesomeIcon data-tip="Detaljert beskrivelse - aktiviteter" className={style.icon} icon="info-circle" color="#007bff" onClick={this.goToMeasure.bind(this)}/></td>
          </tr>
       )
