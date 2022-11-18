3ffb931a7f6fcf5e4bf3d0f7ad00a8172104f8b3
Benjamin Dehli <superelg@gmail.com>
2022-11-18 17:11:28 +0100

    Migrate code for activity table component

--- src/components/partials/ActivityTable.js
+++ src/components/partials/ActivityTable.js
@@ -1,69 +1,70 @@
 // Dependencies
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { GnTable } from '@kartverket/geonorge-web-components/GnTable';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// eslint-disable-next-line no-unused-vars
+import { GnTable } from "@kartverket/geonorge-web-components/GnTable";
 
 // Components
-import ActivityTableRow from 'components/partials/ActivityTable/ActivityTableRow';
-import { fetchOptions } from 'actions/OptionsActions';
-import { translate } from 'actions/ConfigActions';
-
+import ActivityTableRow from "components/partials/ActivityTable/ActivityTableRow";
+import { fetchOptions } from "actions/OptionsActions";
+import { translate } from "actions/ConfigActions";
 
 // Stylesheets
-import style from 'components/partials/ActivityTable.module.scss'
+import style from "components/partials/ActivityTable.module.scss";
 
 const ActivityTable = (props) => {
-   const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-   // State
-   const [dataFetched, setDataFetched] = useState(false);
+    // State
+    const [dataFetched, setDataFetched] = useState(false);
 
-   // Redux store
-   const planStatuses = useSelector((state) => state.options.planStatuses);
+    // Redux store
+    const planStatuses = useSelector((state) => state.options.planStatuses);
 
+    useEffect(() => {
+        dispatch(fetchOptions()).then(() => {
+            setDataFetched(true);
+        });
+    }, [dispatch]);
 
-   useEffect(() => {
-      dispatch(fetchOptions().then(() => {
-         setDataFetched(true);
-      }))
-   }, [dispatch])
+    const renderActivityTableRows = (activities) => {
+        if (!activities?.length) {
+            return (
+                <div className={style.block}>
+                    <p>Tekst mangler</p>
+                </div>
+            );
+        }
 
-   const renderActivityTableRows = (activities) => {
-      if (!activities?.length) {
-         return (
-            <div className={style.block}>
-               <p>Tekst mangler</p>
-            </div>
-         );
-      }
+        return (
+            <gn-table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>NR</th>
+                            <th>{dispatch(translate("Name"))}</th>
+                            <th>{dispatch(translate("Description"))}</th>
+                            <th>{dispatch(translate("Participants"))}</th>
+                            <th>Status</th>
+                            <th>{dispatch(translate("Start"))}</th>
+                            <th>{dispatch(translate("End"))}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {activities.map((activity) => (
+                            <ActivityTableRow key={activity.id} activity={activity} planStatuses={planStatuses} />
+                        ))}
+                    </tbody>
+                </table>
+            </gn-table>
+        );
+    };
 
-      return (
-         <gn-table>
-            <table>
-               <thead>
-                  <tr>
-                     <th>NR</th>
-                     <th>{dispatch(translate('Name'))}</th>
-                     <th>{dispatch(translate('Description'))}</th>
-                     <th>{dispatch(translate('Participants'))}</th>
-                     <th>Status</th>
-                     <th>{dispatch(translate('Start'))}</th>
-                     <th>{dispatch(translate('End'))}</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  {activities.map(activity => <ActivityTableRow key={activity.id} activity={activity} planStatuses={planStatuses} />)}
-               </tbody>
-            </table>
-         </gn-table>
-      );
-   }
+    if (!dataFetched || !props.activities) {
+        return "";
+    }
 
-   if (!dataFetched || !props.activities) {
-      return '';
-   }
-
-   return renderActivityTableRows(props.activities);
-}
+    return renderActivityTableRows(props.activities);
+};
 
 export default ActivityTable;
