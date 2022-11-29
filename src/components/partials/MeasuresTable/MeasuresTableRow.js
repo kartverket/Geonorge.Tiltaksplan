07f69c02e91cdc99fa94a1cebd488582debbfe46
Benjamin Dehli <superelg@gmail.com>
2022-11-29 09:56:03 +0100

    Migrate code for MeasureTableRow component

--- src/components/partials/MeasuresTable/MeasuresTableRow.js
+++ src/components/partials/MeasuresTable/MeasuresTableRow.js
@@ -1,48 +1,69 @@
 // Dependencies
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
-import style from 'components/partials/MeasuresTable/MeasuresTableRow.module.scss';
-import { translate } from 'actions/ConfigActions';
+import style from "components/partials/MeasuresTable/MeasuresTableRow.module.scss";
+import { translate } from "actions/ConfigActions";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MeasuresTableRow = (props) => {
-   
    const dispatch = useDispatch();
-   const navigate = useNavigate();
 
    const getMeasureStatusLabel = (planStatuses, measure) => {
-      return planStatuses.find(status => measure.status === status.value).label;
-   }
+        return planStatuses.find((status) => measure.status === status.value).label;
+    };
 
-
-   const goToMeasure = () => {
-      navigate(`/tiltak/${props.measure.no}`);
-   }
-
-
-   const formatDate =  (date) => {
+    const formatDate = (date) => {
       if (date !== null) {
       const dateArray = date.split("-");
       const day = dateArray[2].substring(0, 4).split("T");
           return day[0] + "." + dateArray[1] + "." + dateArray[0];
       }
-  }
+    };
     
+
       return (                             
          <tr>
-            <td data-label="Nr"><span data-tip="Unikt nummer på tiltaket">{props.measure.no}</span></td>
-            <td data-label={dispatch(translate('Measure'))} style={{paddingRight : 20}}><a href={props.measure.infoUrl} target="_blank" data-tip="Overordnet beskrivelse av tiltaket">{props.measure.name}</a></td>
-            <td data-label="Status" style={{paddingRight : 20}}><span data-tip="Viser fremdrift på tiltaket">{getMeasureStatusLabel(props.planStatuses, props.measure)}</span></td>
-            <td data-label={dispatch(translate('Owner'))}><span data-tip="Hovedansvarlig for gjennomføring av tiltaket">{props.measure.owner.name}</span></td>
-            <td data-label="Sist oppdatert aktivitet"><span data-tip="Sist oppdatert aktivitet/rapport">{formatDate(props.measure.lastUpdatedActivity > props.measure.lastUpdated ? props.measure.lastUpdatedActivity : props.measure.lastUpdated)}</span></td>
-            <td data-label="Link til aktivitet" style={{textAlign: 'center', cursor: 'pointer'}}><FontAwesomeIcon data-tip="Detaljert beskrivelse - aktiviteter" className={style.icon} icon="info-circle" color="#007bff" onClick={goToMeasure}/></td>
-         </tr>
-      )
-   }
+            <td data-label="Nr">
+                <span data-tip="Unikt nummer på tiltaket">{props.measure.no}</span>
+            </td>
+            <td data-label={dispatch(translate("Measure"))} style={{ paddingRight: 20 }}>
+                <a href={props.measure.infoUrl} target="_blank" rel="noreferrer" data-tip="Overordnet beskrivelse av tiltaket">
 
+                    {props.measure.name}
+                </a>
+            </td>
+            <td data-label="Status" style={{ paddingRight: 20 }}>
+                <span data-tip="Viser fremdrift på tiltaket">
+                    {getMeasureStatusLabel(props.planStatuses, props.measure)}
+                </span>
+            </td>
+            <td data-label={dispatch(translate("Owner"))}>
+                <span data-tip="Hovedansvarlig for gjennomføring av tiltaket">{props.measure.owner.name}</span>
+            </td>
+            <td data-label="Sist oppdatert aktivitet">
+                <span data-tip="Sist oppdatert aktivitet/rapport">
+                    {formatDate(
+                        props.measure.lastUpdatedActivity > props.measure.lastUpdated
+                            ? props.measure.lastUpdatedActivity
+                            : props.measure.lastUpdated
+                    )}
+                </span>
+            </td>
+            <td data-label="Link til aktivitet" style={{ textAlign: "center", cursor: "pointer" }}>
+                <Link to={`/tiltak/${props.measure.no}`}>
+                    <FontAwesomeIcon
+                        data-tip="Detaljert beskrivelse - aktiviteter"
+                        className={style.icon}
+                        icon="info-circle"
+                        color="#007bff"
+                    />
+                </Link>
+            </td>
+         </tr>
+    );
+};
 
 export default MeasuresTableRow;
