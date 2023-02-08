bc1322069406faf520f5a1ac0588f55dcd0fccec
Benjamin Dehli <superelg@gmail.com>
2023-02-08 11:32:00 +0100

    Fix issues and implement Geonorge WebComponents for measure table

--- src/components/partials/MeasuresTable.js
+++ src/components/partials/MeasuresTable.js
@@ -3,21 +3,18 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Tooltip } from "react-tooltip";
 // eslint-disable-next-line no-unused-vars
-import { GnTable } from "@kartverket/geonorge-web-components/GnTable";
+import { GnTable, GnSelect } from "@kartverket/geonorge-web-components/GnTable";
 
 // Components
 import MeasuresTableRow from "components/partials/MeasuresTable/MeasuresTableRow";
-import { SelectDropdown } from "components/custom-elements";
 
 // Actions
-import { fetchMeasures } from "actions/MeasuresActions";
 import { fetchOptions } from "actions/OptionsActions";
 import { translate } from "actions/ConfigActions";
 
 // Stylesheets
 import style from "components/partials/MeasuresTable.module.scss";
-import formsStyle from "components/partials/forms.module.scss";
-import 'react-tooltip/dist/react-tooltip.css'
+import "react-tooltip/dist/react-tooltip.css";
 
 const MeasuresTable = (props) => {
     const dispatch = useDispatch();
@@ -25,29 +22,19 @@ const MeasuresTable = (props) => {
     const [dataFetched, setDataFetched] = useState(false);
     const [statusSelected, setStatusSelected] = useState(0);
     const [statuses, setStatuses] = useState();
-    const [measures, setMesaures] = useState();
+    const [sortedMeasures, setSortedMesaures] = useState(props.measures);
     const [sort, setSort] = useState({ column: null, direction: "desc" });
-    const [measuresAll, setMesauresAll] = useState();
 
     // Redux store
     const planStatuses = useSelector((state) => state.options.planStatuses);
 
-    const handleChange = (data) => {
-        let statusMeasures = measuresAll;
-        let status = data.value;
-
-        if (status !== 0) {
-            statusMeasures = statusMeasures.filter(function (el) {
-                return el.status === status;
-            });
-        }
-
-        setStatusSelected(status);
-        setMesaures(statusMeasures);
+    const handleChange = (statusValueString) => {
+        const statusValue = parseInt(statusValueString);
+        setStatusSelected(statusValue);
     };
 
     const getMeasureStatusLabel = (planStatuses, measure) => {
-        return planStatuses.find((status) => measure.status === status.value).label;
+        return planStatuses?.length ? planStatuses.find((status) => measure.status === status.value)?.label : "";
     };
 
     const statusExists = (status, arr) => {
@@ -58,42 +45,18 @@ const MeasuresTable = (props) => {
             : null;
     };
 
-    useEffect(() => {
-        if (!dataFetched) {
-            let fetchedMeasures = [];
-            Promise.all([
-                dispatch(fetchMeasures()).then((response) => {
-                    fetchedMeasures = response.payload;
-                    Promise.resolve();
-                }),
-                dispatch(fetchOptions()).then(() => Promise.resolve())
-            ]).then(() => {
-                setDataFetched(true);
-                setMesaures(fetchedMeasures);
-                setMesauresAll(fetchedMeasures);
-
-                let newPlanStatuses = planStatuses;
-                let allStatus = { value: 0, label: "Alle" };
-                if (!statusExists(0, newPlanStatuses) && !!newPlanStatuses?.length) newPlanStatuses.unshift(allStatus);
-                setStatuses(newPlanStatuses);
-            });
-        }
-    }, [dataFetched, dispatch, planStatuses, props.measures, statuses]);
-
     const setArrow = (column) => {
         let className = "sort-direction";
-
         if (sort.column === column) {
             className += sort.direction === "asc" ? ` ${style.asc}` : ` ${style.desc}`;
         }
-
         return className;
     };
 
     const onSort = (column) => {
         return (e) => {
             const direction = sort.column ? (sort.direction === "asc" ? "desc" : "asc") : "asc";
-            const sortedMeasures = measures.sort((a, b) => {
+            const sortedMeasures = props.measures.sort((a, b) => {
                 if (column === "owner") {
                     const nameA = a.owner.name;
                     const nameB = b.owner.name;
@@ -142,76 +105,112 @@ const MeasuresTable = (props) => {
             if (direction === "desc") {
                 sortedMeasures.reverse();
             }
-            setMesaures(sortedMeasures);
+            setSortedMesaures(sortedMeasures);
             setSort({ column, direction });
         };
     };
 
-    if (!dataFetched) {
-        return "";
-    }
+    useEffect(() => {
+        if (!dataFetched) {
+            dispatch(fetchOptions()).then(() => {
+                setDataFetched(true);
+                let newPlanStatuses = planStatuses;
+                let allStatus = { value: 0, label: "Alle" };
+                if (!statusExists(0, newPlanStatuses) && !!newPlanStatuses?.length) newPlanStatuses.unshift(allStatus);
+                setStatuses(newPlanStatuses);
+            });
+        }
+    }, [dataFetched, dispatch, planStatuses]);
+    
+    useEffect(() => {
+        setSortedMesaures(props.measures);
+    }, [props.measures]);
 
     return (
         <React.Fragment>
             <p>{dispatch(translate("MeasureActivitiesDescription"))}</p>
             <div>Status </div>
-            <div className={formsStyle.comboInput}>
-                {!!statuses?.length}
-                <SelectDropdown
-                    name="status"
-                    value={statusSelected}
-                    options={statuses || []}
-                    onSelect={handleChange}
-                    className={formsStyle.statusSelect}
-                />
-            </div>
+            <gn-select>
+                <select onChange={(event) => handleChange(event.target.value)}>
+                    {statuses?.length
+                        ? statuses.map((status) => {
+                              return (
+                                  <option key={status.value} value={status.value}>
+                                      {status.label}
+                                  </option>
+                              );
+                          })
+                        : ""}
+                </select>
+            </gn-select>
             <gn-table hoverable>
                 <table>
                     <caption>Liste over handlingsplanens tiltak </caption>
                     <thead>
                         <tr>
                             <th style={{ cursor: "pointer" }} onClick={onSort("no")}>
-                                <Tooltip anchorId="measure-no-title"/>
-                                <span id="measure-no-title" data-tooltip-content="Unikt nummer på tiltaket">Nr</span>
+                                <Tooltip anchorId="measure-no-title" />
+                                <span id="measure-no-title" data-tooltip-content="Unikt nummer på tiltaket">
+                                    Nr
+                                </span>
                                 <span className={setArrow("no")}></span>
                             </th>
                             <th style={{ cursor: "pointer" }} onClick={onSort("name")}>
-                                <Tooltip anchorId="measure-name-title"/>
+                                <Tooltip anchorId="measure-name-title" />
                                 <span id="measure-name-title" data-tooltip-content="Overordnet beskrivelse av tiltaket">
                                     {dispatch(translate("Measure"))}
                                 </span>
                                 <span className={setArrow("name")}></span>
                             </th>
                             <th style={{ cursor: "pointer" }} onClick={onSort("status")}>
-                                <Tooltip anchorId="measure-status-title"/>
-                                <span id="measure-status-title" data-tooltip-content="Viser fremdrift på tiltaket">Status</span>
+                                <Tooltip anchorId="measure-status-title" />
+                                <span id="measure-status-title" data-tooltip-content="Viser fremdrift på tiltaket">
+                                    Status
+                                </span>
                                 <span className={setArrow("status")}></span>
                             </th>
                             <th style={{ cursor: "pointer" }} onClick={onSort("owner")}>
-                                <Tooltip anchorId="measure-owner-title"/>
-                                <span id="measure-owner-title" data-tooltip-content="Hovedansvarlig for gjennomføring av tiltaket">
+                                <Tooltip anchorId="measure-owner-title" />
+                                <span
+                                    id="measure-owner-title"
+                                    data-tooltip-content="Hovedansvarlig for gjennomføring av tiltaket"
+                                >
                                     {dispatch(translate("Owner"))}
                                 </span>
                                 <span className={setArrow("owner")}></span>
                             </th>
                             <th style={{ cursor: "pointer" }} onClick={onSort("lastupdated")}>
-                                <Tooltip anchorId="measure-lastUpated-title"/>
-                                <span id="measure-lastUpated-title" data-tooltip-content="Sist oppdatert aktivitet/rapport">Sist&nbsp;oppdatert</span>
+                                <Tooltip anchorId="measure-lastUpated-title" />
+                                <span
+                                    id="measure-lastUpated-title"
+                                    data-tooltip-content="Sist oppdatert aktivitet/rapport"
+                                >
+                                    Sist&nbsp;oppdatert
+                                </span>
                                 <span className={setArrow("lastupdated")}></span>
                             </th>
                             <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {!!measures?.length &&
-                            measures.map((measure) => {
-                                return (
-                                    <MeasuresTableRow key={measure.id} measure={measure} planStatuses={planStatuses} />
-                                );
-                            })}
+                        {!!sortedMeasures?.length &&
+                            sortedMeasures
+                                .filter((measure) => {
+                                    if (statusSelected !== 0) {
+                                        return measure.status === statusSelected;
+                                    } else return true;
+                                })
+                                .map((measure) => {
+                                    return (
+                                        <MeasuresTableRow
+                                            key={measure.id}
+                                            measure={measure}
+                                            planStatuses={planStatuses}
+                                        />
+                                    );
+                                })}
                     </tbody>
                 </table>
-                
             </gn-table>
         </React.Fragment>
     );
